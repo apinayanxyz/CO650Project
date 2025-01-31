f2d111f8dfbd145a3c2744157e36a274581f3ce9
apinayanxyz <akanenthirarasa@gmail.com>
2025-01-31 01:03:12 +0000

    finished

--- Project/stdafx.h
+++ Project/stdafx.h
@@ -16,13 +16,3 @@ enum TStates { On, Off, Broken };
 //class Bulb;
 typedef void(Bulb::* CBP)(); //call-back
 typedef bool(Bulb::* GP)();  // 
-
-
-/*
-class Weapon;
-
-typedef void (Weapon::*CBP)();
-typedef bool (Weapon::*GP)();
-
-enum TStates {Empty, Reload, Loaded, Firing};
-*/
